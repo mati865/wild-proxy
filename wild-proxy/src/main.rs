bb01fdc6fe06fc3f03ca946bae83e86bc65be338
Mateusz Mikuła <oss@mateuszmikula.dev>
2025-12-07 14:22:39 +0100

    refactor: move fallback out of lib.rs

--- wild-proxy/src/main.rs
+++ wild-proxy/src/main.rs
@@ -3,5 +3,5 @@ use anyhow::Result;
 fn main() -> Result<()> {
     // libwild does that right now but probably should not
     // tracing_subscriber::fmt::init();
-    libwild_proxy::fallback()
+    libwild_proxy::fallback::fallback()
 }
